e8f53dc53d61a8f984d36fb770a6e6b40221d6da
Natalia25souza <nataliamariasouza0@gmail.com>
2023-09-18 21:41:44 -0300

    commit

---
+++ Downloads/hotel-alura/hotel-alura/src/com/mchange/v2/c3p0/ComboPooledDataSource.java
@@ -0,0 +1,20 @@
+package com.mchange.v2.c3p0;
+
+public class ComboPooledDataSource {
+
+	public void setJdbcUrl(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setUser(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setPassword(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
